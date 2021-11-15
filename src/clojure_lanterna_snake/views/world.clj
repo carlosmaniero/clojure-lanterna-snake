4bb967795e85fcbc1260338bff41f81bf2fa75f9
Maniero <carlosmaniero@gmail.com>
2021-11-15 13:14:57 -0300

    creates a zebra grid to make it easer to understand the current snake position

--- src/clojure_lanterna_snake/views/world.clj
+++ src/clojure_lanterna_snake/views/world.clj
@@ -3,13 +3,19 @@
             [clojure-lanterna-snake.domain.world :as domain.world]
             [clojure-lanterna-snake.views.screen :as views.screen]))
 
-(defn ^:private create-edge-pixel
+(defn create-edge-pixel
   [position]
 
-  {:pixel/content "░"
+  {:pixel/content "▓"
    :pixel/position position
    :pixel/foreground-color :cyan
    :pixel/background-color :red})
+
+(defn ^:private create-zebra-pixel
+  [position]
+
+  {:pixel/content (if (odd? (+ (:x position) (:y position))) "░" " ")
+   :pixel/position position})
 
 (defn ^:private ->edge
   [axis start size fixed-axis fixed-axis-value]
@@ -18,25 +24,40 @@
        (map inc)
        (map #(create-edge-pixel {axis % fixed-axis fixed-axis-value}))))
 
-(defn ^:private ->top-edge
+(defn ^:private ->top-edge    [world] (->edge :x 1 (:width world) :y 1))
+(defn ^:private ->bottom-edge [world] (->edge :x 1 (:width world) :y (:height world)))
+(defn ^:private ->left-edge   [world] (->edge :y 2 (dec (:height world)) :x 1))
+(defn ^:private ->right-edge  [world] (->edge :y 2 (dec (:height world)) :x (:width world)))
+
+(defn tap
+  [any]
+
+  (do
+    (println any)
+    any))
+
+(defn ->zebra-line
+  [y world]
+
+  (->> (:width world)
+       (dec)
+       (dec)
+       (range)
+       (map inc)
+       (map inc)
+       (map #(create-zebra-pixel {:x % :y y}))))
+
+(defn ->zebra
   [world]
 
-  (->edge :x 1 (:width world) :y 1))
-
-(defn ^:private ->bottom-edge
-  [world]
-
-  (->edge :x 1 (:width world) :y (:height world)))
-
-(defn ^:private ->left-edge
-  [world]
-
-  (->edge :y 2 (dec (:height world)) :x 1))
-
-(defn ^:private ->right-edge
-  [world]
-
-  (->edge :y 2 (dec (:height world)) :x (:width world)))
+  (->> (:height world)
+       (dec)
+       (dec)
+       (range)
+       (map inc)
+       (map inc)
+       (map #(->zebra-line % world))
+       (flatten)))
 
 (s/defn ->view :- [views.screen/Pixel]
   [world :- domain.world/World]
@@ -45,5 +66,6 @@
       (into (->top-edge    world))
       (into (->bottom-edge world))
       (into (->right-edge  world))
-      (into (->left-edge   world))))
+      (into (->left-edge   world))
+      (into (->zebra       world))))
 
--- test/clojure_lanterna_snake/views/world_test.clj
+++ test/clojure_lanterna_snake/views/world_test.clj
@@ -6,17 +6,23 @@
 
 (def my-world {:width 3 :height 3})
 
-(def expected-view [{:pixel/position {:x 1 :y 1}}
-                    {:pixel/position {:x 2 :y 1}}
-                    {:pixel/position {:x 3 :y 1}}
-                    {:pixel/position {:x 1 :y 2}}
-                    {:pixel/position {:x 3 :y 2}}
-                    {:pixel/position {:x 1 :y 3}}
-                    {:pixel/position {:x 2 :y 3}}
-                    {:pixel/position {:x 3 :y 3}}])
+(def expected-view [(views.world/create-edge-pixel {:x 1 :y 1})
+                    (views.world/create-edge-pixel {:x 2 :y 1})
+                    (views.world/create-edge-pixel {:x 3 :y 1})
+                    (views.world/create-edge-pixel {:x 1 :y 2})
+                    (views.world/create-edge-pixel {:x 3 :y 2})
+                    (views.world/create-edge-pixel {:x 1 :y 3})
+                    (views.world/create-edge-pixel {:x 2 :y 3})
+                    (views.world/create-edge-pixel {:x 3 :y 3})])
+
+(defn is-my-world-center-pixel
+  [pixel]
+
+  (= {:x 2 :y 2} (:pixel/position pixel)))
 
 (deftest world->view
   (testing "maps world to view"
-    (let [my-world-pixels (views.world/->view my-world)]
-      (is (match? (m/in-any-order expected-view) my-world-pixels))
-      (is (= 8 (count my-world-pixels))))))
+    (let [my-world-pixels (views.world/->view my-world)
+          center-pixel    (some is-my-world-center-pixel my-world-pixels)]
+      (is (match? (m/embeds expected-view) my-world-pixels))
+      (is (not (= :red (:pixel/background-color center-pixel)))))))
